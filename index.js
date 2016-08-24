33b0288406d031b382f70cdfe2dd02efa4a5db6a
Chandler Van De Water <chandler.vdw@gmail.com>
2016-08-24 16:08:57 -0400

    Update for react-native@0.31 + ES6

--- index.js
+++ index.js
@@ -1,71 +1,56 @@
-'use strict';
+import React, { Component, PropTypes } from 'react';
+import { View, Dimensions, NativeMethodsMixin } from 'react-native';
 
-var React = require('react-native');
-var window = React.Dimensions.get('window');
-var {View, NativeMethodsMixin} = React;
+class InViewPort extends Component {
+  mixins: [NativeMethodsMixin]
 
-module.exports = React.createClass({
-  displayName: 'InViewPort',
-  mixins: [NativeMethodsMixin],
-  propTypes: {
-    onChange: React.PropTypes.func.isRequired,
-    active: React.PropTypes.bool,
-    delay: React.PropTypes.number
-  },
-
-  getDefaultProps: function () {
-    return {
-      active: true,
-      delay: 100
-    };
-  },
-
-  getInitialState: function(){
-    return {
+  constructor() {
+    this.state = {
       rectTop: 0,
       rectBottom: 0
-    }
-  },
-  componentDidMount: function () {
+    };
+  }
+
+  componentDidMount() {
     if (this.props.active) {
       this.startWatching();
     }
-  },
+  }
 
-  componentWillUnmount: function () {
+  componentWillUnmount() {
     this.stopWatching();
-  },
+  }
 
-  componentWillReceiveProps: function (nextProps) {
+  componentWillReceiveProps(nextProps) {
     if (nextProps.active) {
       this.lastValue = null;
       this.startWatching();
     } else {
       this.stopWatching();
     }
-  },
+  }
 
-  startWatching: function () {
+  startWatching() {
     if (this.interval) { return; }
     this.interval = setInterval(this.check, this.props.delay);
-  },
+  }
 
-  stopWatching: function () {
+  stopWatching() {
     this.interval = clearInterval(this.interval);
-  },
-  /**
-   * Check if the element is within the visible viewport
-   */
-  check: function () {
-    var el = this.refs.myview;
-    var rect = el.measure((ox, oy, width, height, pageX, pageY) => {
-      this.setState({
-        rectTop: pageY,
-        rectBottom: pageY + height,
-        rectWidth: pageX + width,
-      })
-    });
-    var isVisible = (
+  }
+
+  // Check if the element is within the visible viewport
+  check() {
+    const el = this.refs.myview,
+          rect = el.measure((ox, oy, width, height, pageX, pageY) => {
+            this.setState({
+              rectTop: pageY,
+              rectBottom: pageY + height,
+              rectWidth: pageX + width,
+            })
+          });
+
+    const isVisible = (
       this.state.rectBottom != 0 && this.state.rectTop >= 0 && this.state.rectBottom <= window.height &&
       this.state.rectWidth > 0 && this.state.rectWidth <= window.width
     );
@@ -75,13 +60,26 @@ module.exports = React.createClass({
       this.lastValue = isVisible;
       this.props.onChange(isVisible);
     }
-  },
+  }
 
-  render: function () {
+  render() {
     return (
       <View ref='myview' {...this.props}>
         {this.props.children}
       </View>
     );
   }
-});
+}
+
+InViewPort.propTypes = {
+  onChange: React.PropTypes.func.isRequired,
+  active: React.PropTypes.bool,
+  delay: React.PropTypes.number
+};
+
+InViewPort.defaultProps = {
+  active: true,
+  delay: 100
+};
+
+export default InViewPort;
